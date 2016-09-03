36d43a49d0e554c4bd5ebb72b18794fd77c20688
spalger <email@spalger.com>
2016-09-02 17:59:48 -0700

    use eslint to prevent syntax errors

--- react.js
+++ react.js
@@ -1,21 +1,24 @@
 module.exports = {
   parser: 'babel-eslint',
   extends: [
-    require.resolve('eslint-config-airbnb/index'),
+    require.resolve('eslint-config-airbnb/base')
   ],
   rules: {
     semi: [2, 'never'],
-    'max-len': ['error', 120]
-    'import/no-reaching-inside': 'error'
-    'import/named': 'error'
-    'import/prefer-default-export': 'off'
-    'no-underscore-dangle': ['error', { allowAfterThis: true }]
-    'react/require-extension': 'off'
-    'react/jsx-filename-extension': ['error', {
-      extensions: [
-        '.js',
-        '.spec.js'
-      ]
-    }]
+    'max-len': ['error', 120],
+    'import/no-reaching-inside': 'error',
+    'import/named': 'error',
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'react/require-extension': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: [
+          '.js',
+          '.spec.js'
+        ]
+      }
+    ]
   }
 }
