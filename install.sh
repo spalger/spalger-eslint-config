bd79df6bfeddf2c734236b13d0e6a94466d65691
spalger <email@spalger.com>
2016-09-02 17:53:50 -0700

    fix install script and react config

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ npm install --save-dev \
   babel-eslint@latest \
   eslint-config-airbnb@latest \
   eslint-plugin-jsx-a11y@latest \
-  eslint-plugin-import@latest \
   eslint-plugin-react@latest \
   @spalger/eslint-config-personal@latest \
   https://github.com/spalger/eslint-plugin-import/releases/download/with-rule-no-reaching-in-2/eslint-plugin-import-1.13.0.tgz \
